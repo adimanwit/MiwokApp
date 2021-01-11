d28167e08214111eead9e9c1660cd7e2287b61e6
Adrian Witaszak <adimanwit@gmail.com>
2021-01-11 23:09:48 +0000

    Added image to the list updated adapter and list_item to show correct images

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -18,22 +18,70 @@ package com.example.android.miwok;
 
 public class Word {
 
+    /** Default translation for the word */
     private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
     private String mMiwokTranslation;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    /** Image resource ID for the word */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
 
+    /** Constant value that represents no image was provided for this word */
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
-    public String getDefaultTranslation() {
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     * @param imageResourceId is the drawable resource ID for the image associated with the word
+     *
+     */
+    public Word(int imageResourceId, String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
 
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
-    public String getMiwokTranslation() {
 
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
 
+    /**
+     * Return the image resource ID of the word.
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
 }
